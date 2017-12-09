6e30d7eadb0bee929397e33800bbebf6d071d107
phlogistinator <jsmnfaye@gmail.com>
2017-12-09 21:25:39 +0800

    added speech recognition

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,34 @@ export class HomePage {
 
   counter: number = 0;
   answers: Array<number> = [];
+  answer: any;
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, private speechRecog: SpeechRecognition) { }
+
+  ngOnInit(){
+    this.speechRecog.hasPermission().then((hasPermission: boolean) => {
+      if(!hasPermission){
+        this.speechRecog.requestPermission().then(
+          () => console.log('Permission granted.'),
+          () => console.log('Permission denied.')
+        )
+      }
+    });
+  }
 
   nextQuestion(){
+    this.speechRecog.startListening().subscribe(
+      (matches: Array<string>) => {
+        this.answer = matches[0];
+        console.log("your answer is "+this.answer);
+        // console.log(this.answers);
+      }
+    )
     this.counter++;
-    this.answers.push(this.counter);
+    this.answers.push(this.answer);
     console.log(this.answers);
+    console.log("answer #"+this.counter);
+    // console.log(this.answer);
   }
 
   resetCounter(){
